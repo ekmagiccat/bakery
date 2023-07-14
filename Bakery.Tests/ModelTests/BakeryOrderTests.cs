95c72d67640a4af7740f58de9d57c8e834c4a70d
Eva Kemp <eva.j.kemp@gmail.com>
2023-07-14 16:48:20 -0700

    good fail on pastry test

--- Bakery.Tests/ModelTests/BakeryOrderTests.cs
+++ Bakery.Tests/ModelTests/BakeryOrderTests.cs
@@ -19,5 +19,20 @@ namespace Bakery.Tests
 
             Assert.AreEqual(breadTotal, result);
         }
+
+        [TestMethod]
+        public void Output_ReturnsPastryOrder()
+        {
+            int UserPastry = 4;
+            int pastryTotal = UserPastry * 2;
+            int freePastries = UserPastry / 4;
+            pastryTotal -= (freePastries * 3);
+            Pastry pastryOutput = new Pastry(UserPastry);
+
+            int result2 = pastryOutput.Output2(UserPastry);
+
+            Assert.AreEqual(pastryTotal, result2);
+
+        }
     }
 }
