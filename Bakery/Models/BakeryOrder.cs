bde13082389b7590b72709505e60123c9516dc0c
Eva Kemp <eva.j.kemp@gmail.com>
2023-07-14 14:12:10 -0700

    start bs logic and tests

--- Bakery/Models/BakeryOrder.cs
+++ Bakery/Models/BakeryOrder.cs
@@ -5,12 +5,30 @@ namespace BakeryOrder.Models
 {
 
     public class Bread
-    { 
-        public int BreadOrder;
-    }
-
-    public class Pastry
     {
-        public int PastryOrder;
+        public int UserBread;
+
+        public int BreadTotal;
+
+        public int Output(int UserBread)
+        {
+            for (int index = 1; index <= UserBread; index++)
+            {
+                if (index % 3 == 0 && index <= 3)
+                {
+                    return BreadTotal - 5;
+                }
+                else (index % 3 != 0);
+                {
+                    return BreadTotal;
+
+                }
+            }
+        }
+
+        public class Pastry
+        {
+            public int PastryOrder;
+        }
     }
 }
--- Bakery/Program.cs
+++ Bakery/Program.cs
@@ -10,10 +10,17 @@ namespace BakeryOrder
             Console.WriteLine("*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*");
             Console.WriteLine("Welcome to (っ◔◡◔)っ ♥ Pierre's Bakery♥ !");
             Console.WriteLine("We have fresh baked breads and pastries for you!");
+            Console.WriteLine("");
             Console.WriteLine("Pricing: Bread - $5 per loaf or buy 2, get 1 free!");
             Console.WriteLine("How many loaves of bread would you like?");
+            Console.WriteLine("Your bread total will be $ ");
 
             Console.WriteLine("Pricing: Pastries - $2 per pastry or buy 3, get 1 free!");
+            Console.WriteLine("How many pastries of would you like?");
+            Console.WriteLine("Your pastry total will be $ ");
+
+            Console.WriteLine("Thank you for your order! Your final total will be $ ");
+
 
         }
     }
