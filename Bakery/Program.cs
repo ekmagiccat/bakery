0ba016d9581d5d6f24d8996ccf64b23d422a7303
Eva Kemp <eva.j.kemp@gmail.com>
2023-07-14 12:06:20 -0700

    add test and bread class to BakeryOrder

---
+++ Bakery/Program.cs
@@ -0,0 +1,17 @@
+using System;
+using BakeryOrder.Models;
+
+namespace BakeryOrder
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*");
+            Console.WriteLine("Welcome to the Pierre's Bakery!");
+            Console.WriteLine("We have fresh baked breads and pastries for you!");
+            Console.WriteLine("How many loaves of bread would you like?");
+
+        }
+    }
+}
